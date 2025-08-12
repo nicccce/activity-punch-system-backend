c37490dc129b122083a33c44ec3a2b712feee84d
Hanserprpr <2041283903@qq.com>
2025-08-12 17:55:27 +0800

    resolves #2 牛的，密码还能返回的

--- internal/model/user.go
+++ internal/model/user.go
@@ -6,5 +6,6 @@ type User struct {
 	RoleID    int    `gorm:"default:0;not null" json:"role_id"`
 	NickName  string `gorm:"type:varchar(20);not null" json:"nick_name"`
 	Avatar    string `gorm:"type:varchar(255);" json:"avatar"`
-	Password  string `gorm:"type:varchar(255);" json:"password"`
+	// Password 不对外返回，仅用于登录校验与修改密码
+	Password string `gorm:"type:varchar(255);" json:"-"`
 }
