c233beff5a7c90383e702a6e4c18409774ba39a0
NIA-sai <zsgbp@outlook.com>
2026-01-26 01:52:07 +0800

    bugfix

--- internal/module/stats/activity/sql.go
+++ internal/module/stats/activity/sql.go
@@ -112,12 +112,17 @@ func briefStats(activityID, userID uint, columnIDs []uint, askTime int64, result
 	}
 
 	var totalScoreResult briefResult
-	if err := database.DB.Table("total_score").
-		Select(`
+	if err := database.DB.
+		Table("(?) as t", database.DB.
+			Table("total_score").
+			Select(`
+			user_id,
 			score AS ts,
-            RANK() OVER (ORDER BY score DESC) AS ranks
-        `).
-		Where("activity_id = ? AND user_id = ?", activityID, userID).
+			RANK() OVER (ORDER BY score DESC) AS ranks
+		`).
+			Where("activity_id = ?", activityID),
+		).
+		Where("user_id = ?", userID).
 		Scan(&totalScoreResult).Error; err != nil {
 		log.Error("数据库 查询total_score失败", "error", err.Error())
 		return err
